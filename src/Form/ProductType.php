ab60e178453a05d00df8cb4ab3af9634144914e1
Hervé champagne <hchampagne56@gmail.com>
2021-12-02 22:39:59 +0100

    fonctions avanés du composant form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,14 +4,17 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Product;
-
+use App\Form\DataTransformer\CentimesTransformer;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
@@ -28,7 +31,8 @@ class ProductType extends AbstractType
             ])
             ->add('price', MoneyType::class, [
                 'label' => 'Prix du Prduit en ',
-                'attr' => ['placeholder' => 'Tapez le prix du produit en €']
+                'attr' => ['placeholder' => 'Tapez le prix du produit en €'],
+                'divisor' => '100'
             ])
             ->add('mainPicture', UrlType::class, [
                 'label' => 'image du produit',
@@ -46,6 +50,35 @@ class ProductType extends AbstractType
                     }
                 ]
             );
+
+        //$builder->get('price')->addModelTransformer(new CentimesTransformer);
+
+
+        // $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+
+        //    $form = $event->getForm();
+
+        //    /** @var Product */
+        //$product = $form->getData();
+
+        //dd($product);
+
+        //     if ($product->getId() === null) {
+
+        //         $form->add(
+        //             'category',
+        //             EntityType::class,
+        //             [
+        //                 'label' => 'Catégorie',
+        //                 'placeholder' => '--choisr une catégorie--',
+        //                 'class' => Category::class,
+        //                 'choice_label' => function (Category $category) {
+        //                     return strtoupper($category->getName());
+        //                 }
+        //             ]
+        //         );
+        //     }
+        //});
     }
 
     public function configureOptions(OptionsResolver $resolver): void
